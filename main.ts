ba6ceb5d1f955da257d14233511cf8495ca588ea
SYM0007 <134348674+SYM0007@users.noreply.github.com>
2023-05-23 12:05:50 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("F D G E B C5 E G ", 120)
+})
+input.onButtonPressed(Button.B, function () {
+    music.playMelody("D E C F C F G F ", 452)
 })
